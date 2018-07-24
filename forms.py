886ff6f16949b190339169fbed367a9c240ed0dd
Luke <greendale2003@gmail.com>
2018-07-23 20:27:56 -0500

    correct typos in forms

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms.validators import Length, DataRequired, EqualTo, Email
 
 class RegistrationForm(Form):
     username = StringField('Username', [Length(min=4, max=20)])
-    email = StringField('Email Adress', [Email(), DataRequired()])
-    password = PasswordField('New Password', [DataRequired(),
+    email = StringField('Email Address', [Email(), DataRequired()])
+    password = PasswordField('Password', [DataRequired(),
     EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Repeat Password')
+    confirm = PasswordField('Confirm Password')
