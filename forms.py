1d5a4287cb1d794ef9f618445d734811774a7c0e
Luke <greendale2003@gmail.com>
2018-08-03 12:46:35 -0500

    Half-Add Database Functionality

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from wtforms import Form, StringField, PasswordField
 from wtforms.validators import Length, DataRequired, EqualTo, Email
 
+
 class RegistrationForm(Form):
-    username = StringField('Username', [Length(min=4, max=20)])
+    username = StringField('Username', [Length(min=4, max=30)])
     email = StringField('Email Address', [Email(), DataRequired()])
     password = PasswordField('Password', [DataRequired(),
-    EqualTo('confirm', message='Passwords must match')])
+    EqualTo('confirm', message='Passwords must match'), Length(min=2, max=60)])
     confirm = PasswordField('Confirm Password')
---
+++ testfile.py
@@ -0,0 +1,2 @@
+import sqlite3
+conn = sqlite3.connect('\\Users\\Luke\\Documents\\GitHub\\Calendar-app\\user_db.sqlite')
